dbd472ddd1245010b56044d617a03bac3530d020
arnav dhamija <arnav.dhamija@gmail.com>
2018-02-23 00:47:43 +0530

    Some work done

--- app/src/main/java/com/arnavdhamija/roamnet/MainActivity.java
+++ app/src/main/java/com/arnavdhamija/roamnet/MainActivity.java
@@ -12,6 +12,8 @@ import android.support.v4.util.SimpleArrayMap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,18 +28,26 @@ import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
 import com.google.android.gms.nearby.connection.DiscoveryOptions;
 import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
 import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
 import com.google.android.gms.nearby.connection.Strategy;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.jaredrummler.android.device.DeviceName;
 
+import java.io.File;
+import java.sql.Connection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     private ConnectionsClient mConnectionClient;
     private NotificationManager mNotificationManager;
     private String connectedEndpoint;
+    private boolean connectionActive = false;
 
     final String TAG = "Roamnet";
-    final String deviceId = "Roamnet_" + DeviceName.getDeviceInfo(this).model;
+    String deviceId;
 
     private final SimpleArrayMap<Long, NotificationCompat.Builder> incomingPayloads = new SimpleArrayMap<>();
     private final SimpleArrayMap<Long, NotificationCompat.Builder> outgoingPayloads = new SimpleArrayMap<>();
@@ -66,14 +76,79 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getPermissions();
 
+        deviceId = "Roamnet_" + DeviceName.getDeviceName();
+        customLogger(deviceId);
         mConnectionClient = Nearby.getConnectionsClient(this);
         mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+        TextView deviceIdView = findViewById(R.id.deviceIdView);
+        deviceIdView.setText("Device ID: " + deviceId);
+
+        final Button toggleRoamnetButton = findViewById(R.id.toggleRoamnet);
+        toggleRoamnetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!connectionActive) {
+                    startAdvertising();
+                    startDiscovery();
+                    toggleRoamnetButton.setText("Stop Roamnet");
+                    connectionActive = true;
+                } else {
+                    mConnectionClient.stopDiscovery();
+                    mConnectionClient.stopAdvertising();
+                    mConnectionClient.stopAllEndpoints();
+                    toggleRoamnetButton.setText("Start Roamnet");
+                    connectionActive = false;
+                }
+            }
+        });
+        Button logClearButton = findViewById(R.id.logClearButton);
+        logClearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView logView = findViewById(R.id.logView);
+                logView.setText("");
+            }
+        });
+    }
+
+    private void customLogger(String msg) {
+        Log.d(TAG, msg);
+        TextView logView = findViewById(R.id.logView);
+        String timeStamp = new SimpleDateFormat("HH.mm.ss").format(new Date());
+        logView.append(timeStamp+' '+msg+"\n");
+    }
+
+    private void restartNearby() {
+        customLogger("RestartingNearby");
+        mConnectionClient.stopAdvertising();
+        mConnectionClient.stopDiscovery();
+        startAdvertising();
+        startDiscovery();
+    }
+
+    private NotificationCompat.Builder buildNotification(Payload payload, boolean isIncoming) {
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this).setContentTitle(isIncoming ? "Receiving..." : "Sending...").setSmallIcon(R.drawable.common_full_open_on_phone);
+        long size = payload.asFile().getSize();
+        boolean indeterminate = false;
+        if (size == -1) {
+            // This is a stream payload, so we don't know the size ahead of time.
+            size = 100;
+            indeterminate = true;
+        }
+        notification.setProgress((int)size, 0, indeterminate);
+        return notification;
     }
 
     private void startAdvertising() {
@@ -86,14 +161,14 @@ public class MainActivity extends AppCompatActivity {
                         new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void unusedResult) {
-                                Log.d(TAG, "Advertising Go!");
+                                customLogger("Advertising Go!");
                             }
                         })
                 .addOnFailureListener(
                         new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
-                                Log.d(TAG, "Advert fail");
+                                customLogger("Advert fail");
                                 // We were unable to start advertising.
                             }
                         });
@@ -108,7 +183,7 @@ public class MainActivity extends AppCompatActivity {
                         new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void unusedResult) {
-                                Log.d(TAG, "Discovery go!");
+                                customLogger("Discovery go!");
                                 // We're discovering!
                             }
                         })
@@ -126,70 +201,75 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onEndpointFound(
                         String endpointId, DiscoveredEndpointInfo discoveredEndpointInfo) {
-                    Log.d(TAG, "FOUND ENDPOINT: " + endpointId + "Info " + discoveredEndpointInfo.getEndpointName() + " id " + discoveredEndpointInfo.getServiceId());
-                    mConnectionClient.stopAdvertising();
-                    mConnectionClient.stopDiscovery();
-                    Log.d(TAG, "Stopping before requesting Conn");
-                    mConnectionClient.requestConnection(
-                            deviceId,
-                            endpointId,
-                            mConnectionLifecycleCallback).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Log.d(TAG, "requesting conn");
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d(TAG, "fail conn t_t" + e.getMessage());
-                            startAdvertising();
-                            startDiscovery();
-                        }
-                    });
+                    customLogger("FOUND ENDPOINT: " + endpointId + "Info " + discoveredEndpointInfo.getEndpointName() + " id " + discoveredEndpointInfo.getServiceId());
+                    if (discoveredEndpointInfo.getEndpointName().startsWith("Roamnet")) {
+                        mConnectionClient.stopAdvertising();
+                        mConnectionClient.stopDiscovery();
+                        customLogger("Stopping before requesting Conn");
+                        mConnectionClient.requestConnection(
+                                deviceId,
+                                endpointId,
+                                mConnectionLifecycleCallback).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                customLogger("requesting conn");
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                customLogger("fail conn t_t" + e.getMessage());
+                                startAdvertising();
+                                startDiscovery();
+                            }
+                        });
+                    }
                 }
 
                 @Override
                 public void onEndpointLost(String endpointId) {
-                    Log.d(TAG, "lost ENDPOINT: " + endpointId);
+                    customLogger("lost ENDPOINT: " + endpointId);
                 }
             };
 
     private final ConnectionLifecycleCallback mConnectionLifecycleCallback =
             new ConnectionLifecycleCallback() {
-
+                String endpointName;
                 @Override
                 public void onConnectionInitiated(
                         String endpointId, ConnectionInfo connectionInfo) {
                     // Automatically accept the connection on both sides.
-                    Log.d(TAG, "Connection initated");
-                    mConnectionClient.acceptConnection(endpointId, mPayloadCallback);
+                    endpointName = connectionInfo.getEndpointName();
+                    if (endpointName.startsWith("Roamnet")) {
+                        customLogger("Connection initated w/ " + endpointName);
+                        mConnectionClient.acceptConnection(endpointId, mPayloadCallback);
+                    }
                 }
 
                 @Override
                 public void onConnectionResult(String endpointId, ConnectionResolution result) {
                     switch (result.getStatus().getStatusCode()) {
                         case ConnectionsStatusCodes.STATUS_OK:
-                            Log.d(TAG, "GGWP! :D:D:D");
+                            customLogger("GGWP! :D:D:D");
                             Toast.makeText(getApplicationContext(), "Connection Established", Toast.LENGTH_LONG).show();
-//                            TextView textView = (TextView) findViewById(R.id.textbox0);
-//                            textView.setText("Connection Established with " + endpointId);
+                            TextView textView = (TextView) findViewById(R.id.connectionStatusView);
+                            textView.setText("Connected To: " + endpointId);
                             connectedEndpoint = endpointId;
-                            Log.d(TAG, "stopping AD");
+                            customLogger("stopping AD");
                             mConnectionClient.stopAdvertising();
                             mConnectionClient.stopDiscovery();
-                            Log.d(TAG, "stopped AD!!");
+                            customLogger("stopped AD!!");
                             // We're connected! Can now start sending and receiving data.
                             break;
                         case ConnectionsStatusCodes.STATUS_ENDPOINT_IO_ERROR: //this code is ignored
-                            Log.d(TAG, "endpt error, restart");
+                            customLogger("endpt error, restart");
                             restartNearby();
                             break;
                         case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
                             // The connection was rejected by one or both sides.
-                            Log.d(TAG, "fail D:");
+                            customLogger("fail D:");
                             break;
                         case ConnectionsStatusCodes.STATUS_ERROR:
-                            Log.d(TAG, "bigfail");
+                            customLogger("bigfail");
                             // The connection broke before it was able to be accepted.
                             break;
                     }
@@ -199,19 +279,96 @@ public class MainActivity extends AppCompatActivity {
                 public void onDisconnected(String endpointId) {
                     // We've been disconnected from this endpoint. No more data can be
                     // sent or received.
-                    Log.d(TAG, "connection terminated, find a way to autostart");
-//                    TextView textView = (TextView) findViewById(R.id.textbox0);
-//                    textView.setText("disconnected from " + endpointId);
+                    customLogger("connection terminated, find a way to autostart");
+                    TextView textView = (TextView) findViewById(R.id.connectionStatusView);
+                    textView.setText("Disconnected");
                     restartNearby();
                 }
             };
 
-    private void restartNearby() {
-        Log.d(TAG, "RestartingNearby");
-        mConnectionClient.stopAdvertising();
-        mConnectionClient.stopDiscovery();
-        startAdvertising();
-        startDiscovery();
+    private void addPayloadFilename(String payloadFilenameMessage) {
+        int colonIndex = payloadFilenameMessage.indexOf(':');
+        String payloadId = payloadFilenameMessage.substring(0, colonIndex);
+        String filename = payloadFilenameMessage.substring(colonIndex + 1);
+        filePayloadFilenames.put(Long.valueOf(payloadId), filename);
     }
 
+    private final PayloadCallback mPayloadCallback =
+            new PayloadCallback() {
+                @Override
+                public void onPayloadReceived(String endpointId, Payload payload) {
+                    customLogger("getting a payload");
+                    if (payload.getType() == Payload.Type.BYTES) {
+                        try {
+                            String payloadFilenameMessage = new String(payload.asBytes(), "UTF-8");
+                            addPayloadFilename(payloadFilenameMessage);
+                            customLogger("Getting a byte pyalod" + payloadFilenameMessage);
+//                            addPayloadFilename(payloadFilenameMessage);
+                        } catch (Exception e) {
+
+                        }
+                    } else if (payload.getType() == Payload.Type.FILE) {
+                        customLogger("Getting a file pyalod " + payload.asFile().getSize());
+                        NotificationCompat.Builder notification = buildNotification(payload, true /*isIncoming*/);
+                        mNotificationManager.notify((int) payload.getId(), notification.build());
+                        incomingPayloads.put(Long.valueOf(payload.getId()), notification);
+                        incomingPayloadReferences.put(payload.getId(), payload);
+                    }
+                }
+
+                @Override
+                public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
+                    NotificationCompat.Builder notification = new NotificationCompat.Builder(getApplicationContext()).setSmallIcon(R.drawable.common_full_open_on_phone);
+                    long payloadId = update.getPayloadId();
+                    if (incomingPayloads.containsKey(payloadId)) {
+                        notification = incomingPayloads.get(payloadId);
+                        if (update.getStatus() != PayloadTransferUpdate.Status.IN_PROGRESS) {
+                            // This is the last update, so we no longer need to keep track of this notification.
+                            incomingPayloads.remove(payloadId);
+                        }
+                    } else if (outgoingPayloads.containsKey(payloadId)) {
+                        notification = outgoingPayloads.get(payloadId);
+                        if (update.getStatus() != PayloadTransferUpdate.Status.IN_PROGRESS) {
+                            // This is the last update, so we no longer need to keep track of this notification.
+                            outgoingPayloads.remove(payloadId);
+                        }
+                    }
+                    Payload payload = incomingPayloadReferences.get(update.getPayloadId());
+                    switch(update.getStatus()) {
+                        case PayloadTransferUpdate.Status.IN_PROGRESS:
+                            int size = (int)update.getTotalBytes();
+//                            customLogger("Bytes transferred " + update.getBytesTransferred());
+                            if (size == -1) {
+                                // This is a stream payload, so we don't need to update anything at this point.
+                                return;
+                            }
+                            notification.setProgress(size, (int)update.getBytesTransferred(), false /* indeterminate */);
+                            break;
+                        case PayloadTransferUpdate.Status.SUCCESS:
+                            // SUCCESS always means that we transferred 100%.
+                            notification
+                                    .setProgress(100, 100, false /* indeterminate */)
+                                    .setContentText("Transfer complete!");
+                            customLogger("Transfer done");
+                            String filename = filePayloadFilenames.remove(update.getPayloadId());
+                            if (payload != null) {
+                                File payloadFile = payload.asFile().asJavaFile();
+                                payloadFile.renameTo(new File(payloadFile.getParentFile(), filename));
+                                customLogger("found N renamed");
+                            } else {
+                                customLogger("NUll");
+                            }
+                            break;
+                        case PayloadTransferUpdate.Status.FAILURE:
+                            notification
+                                    .setProgress(0, 0, false)
+                                    .setContentText("Transfer failed");
+                            break;
+                    }
+//                    if (payload != null) {
+                    mNotificationManager.notify((int) payloadId, notification.build());
+//                    }
+                }
+
+            };
 }
