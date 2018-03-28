736192d3fedbe9f2568fa067ed310179af7cacb9
Abhishek Thakur <atst@live.com>
2018-03-28 16:26:58 +0530

    added quantization values and fixed the debug output to seperate files

--- server/scripts/cron_wrapper.sh
+++ server/scripts/cron_wrapper.sh
@@ -3,8 +3,11 @@
 # this file is a wrapper around 2L or 4L script for SHM
 
 MY_PATH="`dirname \"$0\"`"
-echo "$MY_PATH" >> /var/spool/vector/run_out
+video_file_counter=`cat /var/spool/vector/counter`
+counterPart=`printf "%05d" ${video_file_counter}`
+
+echo "$MY_PATH" >> /var/spool/vector/run_out_${counterPart}
 date >> /var/spool/vector/run_out
 
-bash ${MY_PATH}/video_record_2L.sh >> /var/spool/vector/run_out 2>&1
+bash ${MY_PATH}/video_record_2L.sh >> /var/spool/vector/run_out_${counterPart} 2>&1
 
