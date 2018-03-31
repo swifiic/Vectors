727458b1399a1738915b17eb2830e5328381f3c6
arnav dhamija <arnav.dhamija@gmail.com>
2018-03-31 23:43:10 +0530

    Added ConnectionLog class

---
+++ common/src/main/java/com/arnavdhamija/common/ConnectionLog.java
@@ -0,0 +1,42 @@
+package com.arnavdhamija.common;
+
+import android.os.SystemClock;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import java.time.Clock;
+import java.util.ArrayList;
+
+public class ConnectionLog {
+    private final static String TAG = "ConnectionLog";
+    private String source;
+    private String destination;
+    private long connectionStartedTime;
+    private long connectionTerminatedTime;
+    ArrayList<String> filesSent;
+    ArrayList<String> filesReceived;
+
+    ConnectionLog(String source, String destination) {
+        this.source = source;
+        this.destination = destination;
+        filesSent = new ArrayList<>();
+        filesReceived = new ArrayList<>();
+        connectionStartedTime = System.currentTimeMillis()/1000;
+    }
+
+    void connectionTerminated() {
+        connectionTerminatedTime = System.currentTimeMillis()/1000;
+    }
+
+    public static ConnectionLog fromString(String jsonString) {
+        Gson gson = new Gson();
+        return gson.fromJson(jsonString, ConnectionLog.class);
+    }
+
+    public String toString() {
+        Gson gson = new Gson();
+        Log.d(TAG, "JSON string " + gson.toJson(this));
+        return gson.toJson(this);
+    }
+}
