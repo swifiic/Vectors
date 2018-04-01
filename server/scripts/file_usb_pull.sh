fb71da943b28d1b0a549af7063fe5b5261551c2d
Your Name <you@example.com>
2018-04-02 01:23:45 +0530

    adb based pull - file path name is not automatically checked

--- server/scripts/file_usb_pull.sh
+++ server/scripts/file_usb_pull.sh
@@ -2,23 +2,22 @@
 
 # this file by cron wrapper called after layer files are generated
 
-tgtdir=`ls -d /run/user/*/gvfs/mtp*/Inter*/RoamnetData | head -n 1`
-TgtDir=$(printf %q "$tgtdir")
+tgtdir=`/usr/bin/adb shell "ls /sdcard/RoamnetData/ ; exit 0" | head -n 1`
 
 dest_fldr="/var/www/video_in"
 import="${dest_fldr}/import"
 newLine="
 "
-ls -l "${tgtdir}"
 timeAtDest=`date +%s`
-mv "${tgtdir}"/video* "${import}"
+/usr/bin/adb pull /sdcard/RoamnetData/ ${import}
 rcvdFiles=`ls ${import}/video*| grep -v json`
 outStr=""
 for file in ${rcvdFiles} ; do
    mv ${file} ${dest_fldr}
    mv ${file}.json ${dest_fldr}
-   echo "Moved ${file}"
    fileName=$(basename ${file})
+   /usr/bin/adb shell "rm /sdcard/RoamnetData/${fileName}*; exit 0"
+   echo "Moved ${file}"
    if [ -f "${dest_fldr}/${fileName}" ] ; then
        outStr=$"${outStr}${newLine}{\"filename\":\"${fileName}\",\"time\":${timeAtDest}},"
    else
@@ -30,12 +29,15 @@ echo "${outStr}" | cat - ${dest_fldr}/rcvdlist.txt > /tmp//rcvdlist.txt.temp && 
 subStr=`head -n 500 ${dest_fldr}/rcvdlist.txt | grep -v "^$"`
 str2=${subStr::-1} ; # remove the last ","
 
-echo "{\"ack_time\":${timeAtDest},"items":[${str2}]}" > "${tgtdir}"/ack_video_00.json
+echo "{\"ack_time\":${timeAtDest},"items":[${str2}]}" > /tmp/ack_video_00.json
+/usr/bin/adb push /tmp/ack_video_00.json /sdcard/RoamnetData/
+if [ "$?" -ne "0" ]; then
+      echo "Push of ack may have failed"
+fi
 echo "Generated Ack as # {\"ack_time\":${timeAtDest},"items":[${str2}]} #"
 
 # {"ack_time":1522417102585,"items":[{"filename":"md_00063.md","time":1522417102585},{"filename":"md_00062.md","time":1522417102585},{"filename":"video_00096_L0T1.out","time":1522417102585},{"filename":"md_00061.md","time":1522417102585},{"filename":"md_00096.md","time":1522417102585}]}
 
-ls "${tgtdir}"/video_${counterPart}* "${tgtdir}"/ack*
 
 # date >> /var/spool/vector/run_out
 
