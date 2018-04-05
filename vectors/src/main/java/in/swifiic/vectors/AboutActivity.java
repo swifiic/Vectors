0fa595dc37c8af575d2323fe98909e3f9b9d49fc
arnav dhamija <arnav.dhamija@gmail.com>
2018-04-05 11:32:58 +0530

    Adding links

--- vectors/src/main/java/in/swifiic/vectors/AboutActivity.java
+++ vectors/src/main/java/in/swifiic/vectors/AboutActivity.java
@@ -2,6 +2,8 @@ package in.swifiic.vectors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+//        TextView swifiicLinkText = findViewById(R.id.swifiicLink);
+//        TextView fiveHunderedYearsText = findViewById(R.id.fiveHundredLink);
+//        Linkify.addLinks(swifiicLinkText, Linkify.ALL);
     }
 }
