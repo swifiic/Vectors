05ef932006ffb7d58b6e47d507ccb1aa87c61862
arnav dhamija <arnav.dhamija@gmail.com>
2018-04-05 11:46:41 +0530

    Added about activity

--- vectors/src/main/java/in/swifiic/vectors/AboutActivity.java
+++ vectors/src/main/java/in/swifiic/vectors/AboutActivity.java
@@ -2,6 +2,7 @@ package in.swifiic.vectors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
 import android.widget.TextView;
 
@@ -11,8 +12,11 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-//        TextView swifiicLinkText = findViewById(R.id.swifiicLink);
-//        TextView fiveHunderedYearsText = findViewById(R.id.fiveHundredLink);
+        TextView swifiicLinkText = findViewById(R.id.swifiicLink);
+        TextView fiveHunderedYearsText = findViewById(R.id.fiveHundredLink);
+        swifiicLinkText.setMovementMethod(LinkMovementMethod.getInstance());
+        fiveHunderedYearsText.setMovementMethod(LinkMovementMethod.getInstance());
 //        Linkify.addLinks(swifiicLinkText, Linkify.ALL);
+//        Linkify.addLinks(fiveHunderedYearsText, Linkify.ALL);
     }
 }
