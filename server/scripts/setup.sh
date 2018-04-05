fc90a7c61abd1353f51b8d233af74439ef4885f2
vectors <vectors@hyderabad.bits-pilani.ac.in>
2018-04-05 12:32:46 +0530

    cleaned up a bit to ensure better error reporting

--- server/scripts/setup.sh
+++ server/scripts/setup.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$#" -ne 3 ]; then
-    echo "Usage: setup.sh /path/to/vectors/server /path/to/SHMFolder /path/to/JSVMFolder"
+    echo "Usage: bash setup.sh /path/to/vectors/server /path/to/SHMFolder /path/to/JSVMFolder"
+    echo "   e.g. bash setup.sh /home/vectors/git/vectors/server/ ~/SHM-12.4 ~/JSVM-master/JSVM"
+
     exit
 fi
 
@@ -16,23 +18,28 @@ path=${pToRepo}
 mkdir -p ${filesBase}/bin ${filesBase}/scripts ${filesBase}/rec ${filesBase}/php
 
 cp -r ${path}/cfg ${path}/scripts ${path}/php ${filesBase}/
+echo 0 > ${filesBase}/counter
+touch /var/www/video_in/rcvdlist.txt
+
+echo "Starting compilation "
 cd ${path}/src
 make -j`nproc`
-cp ${path}/src/CombineLS ${path}/src/ExtractAddLS ${filesBase}/bin
 cd -
-
-echo 0 > ${filesBase}/counter
 
 cd ${pToSHM}/build/linux
 make -j`nproc`
-cp ${pToSHM}/bin/*Static* ${filesBase}/bin
 cd -
 
 cd ${pToJSVM}/H264Extension/build/linux
 make -j`nproc`
-cp ${pToJSVM}/../bin/DownConvertStatic* ${filesBase}/bin
 cd -
 
-touch /var/www/video_in/rcvdlist.txt
+echo "Finished compilation. Now deploying the binaries"
+cp ${pToSHM}/bin/*Static* ${filesBase}/bin
+echo "Deployed SHM binaries"
+cp ${pToJSVM}/../bin/DownConvertStatic* ${filesBase}/bin
+echo "Deployed JSVM binaries"
+cp ${path}/src/CombineLS ${path}/src/ExtractAddLS ${filesBase}/bin
+echo "Deployed Vectors binaries"
 
 chmod a+x ${filesBase}/scripts/*
