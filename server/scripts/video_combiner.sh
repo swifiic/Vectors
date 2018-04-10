21f4340636dbbbd88e43c4d0203a3db038884fd8
TejeshwarReddy <tejeshwarreddy1996@gmail.com>
2018-04-11 02:02:07 +0530

    adds changes to video_combiner.sh

--- server/scripts/video_combiner.sh
+++ server/scripts/video_combiner.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-combinerCommand="/var/spool/vector/bin/CombineLS video_${fileCounter} /var/www/video_in/encoded/enc_${fileCounter}.bin 5 2"
-
-# getFiveOldFiles=`find ./ -type f -printf '%T+ %p\n' | sort | head -n 5`
-
 mkdir -p /var/www/video_in/combined
 
 mkdir -p /var/www/video_in/encoded
@@ -14,27 +10,45 @@ mkdir -p /var/www/video_in/todecode
 
 cd /var/www/video_in/
 
-if [ -f video* ]; then
-    mv video* ../todecode
-fi
+cp ./video* ./todecode
 
-cd ../todecode
+cd ./todecode
 
 files=()
 
-oldestMDFile=`find ./ -maxdepth 1 -type f -name "video*.md" -printf '%T@ %p\0' | sort -zn | sed -zn '1s/[0-9,\.]\+ //p'`
+seqNumStr=""
 
-# oldestOutFile=`find ./ -maxdepth 1 -type f -name "video*.out" -printf '%T@ %p\0' | sort -zn | sed -zn '1s/[0-9,\.]\+ //p'`
+while [[ true ]]; do
+    oldestMDFile=`find ./ -maxdepth 1 -type f -name "video*.md" -printf '%T@ %p\0' | sort -zn | sed -zn '1s/[0-9,\.]\+ //p'`
 
-seqNumStr=$(echo ${oldestMDFile}| cut -d'_' -f 2)
+    echo ${oldestMDFile}
 
-seqNumInt=`expr $seqNum + 0`
+    tmp=${oldestMDFile#*_}   # remove prefix ending in "_"
+    seqNumStr=${tmp%.*}   # remove suffix starting with "."
+
+    echo "seq number = ${seqNumStr}"
+
+    echo video_${seqNumStr}_L0T1.out
+    if [[ -e video_${seqNumStr}_L0T1.out ]]; then
+        break
+    else
+        rm video_${seqNumStr}*
+        echo "hello"
+    fi
+done
+
+seqNumInt=`expr $seqNumStr + 0`
+echo ${seqNumInt}
 
 for (( i = 0; i < 5; i++ )); do
     counterPart=`printf "%05d" ${seqNumInt}`
-    files+=(counterPart)
-    seqNumInt++;
+    files+=(${counterPart})
+    ((seqNumInt++));
 done
+
+echo "files array elements"
+
+printf '%s\n' "${files[@]}"
 
 newYUVFile=$(printf "_%s" "${files[@]}")
 newYUVFile=${newYUVFile:1}
@@ -43,26 +57,32 @@ echo $newYUVFile
 
 addFirstFrame=0
 
+numFramesToAdd=32
+
+count=0
+
+echo "num values in array = ${#files[*]}"
+
 for (( i = 0; i < ${#files[*]}; ++ i ))
 do
    echo ${files[$i]}
-   if [[ -f video_${files[$i]}_L0T1* && -f video_${files[$i]}.md ]]; then
+   if [[ -f video_${files[$i]}_L0T1.out && -f video_${files[$i]}.md ]]; then
 
        /var/spool/vector/bin/CombineLS video_${files[$i]} /var/www/video_in/encoded/enc_${files[$i]}.bin 5 2
 
        /var/spool/vector/bin/TAppDecoderStaticd -b /var/www/video_in/encoded/enc_${files[$i]}.bin -ls 2 -o0 /var/www/video_in/decoded/out_${files[$i]}_Q.yuv
 
-       if [[ addFirstFrame == 1 ]]; then
+       if [[ 1 == ${addFirstFrame} ]]; then
            echo 'extending the first frame'
-           ffmpeg -f rawvideo -framerate 5 -s 640x480 -pixel_format yuyv422 -i out_${files[$i-1]}_Q.yuv -c copy -f segment -segment_time 1 frames%d.yuv
-           lastFrame=`ls -At frames* | tail -n 1`
+           firstFrame=`ls -At frames* | tail -n 1`
 
-           for (( i=1; i <= 32; ++i ))
+           for (( j=1; j <= ${numFramesToAdd}; ++j ))
            do
-               cat lastFrame >> $newYUVFile
+               cat firstFrame >> $newYUVFile
            done
            rm frames*
            addFirstFrame=0
+           numFramesToAdd=32
        fi
 
        cat /var/www/video_in/decoded/out_${files[$i]}_Q.yuv >> $newYUVFile
@@ -75,22 +95,32 @@ do
        ffmpeg -f rawvideo -framerate 5 -s 640x480 -pixel_format yuyv422 -i out_${files[$i-1]}_Q.yuv -c copy -f segment -segment_time 1 frames%d.yuv
        lastFrame=`ls -Art frames* | tail -n 1`
 
-       for (( i=1; i <= 32; ++i ))
+       for (( k = i+1; k < ${#files[*]}; k++ )); do
+           if [[ ! -f video_${files[$i]}_L0T1* && -f video_${files[$i]}.md  ]]; then
+               numFramesToAdd+=${numFramesToAdd}
+               ((count++))
+           else
+               break
+           fi
+       done
+
+       for (( j=1; j <= ${numFramesToAdd}; ++j ))
        do
            cat lastFrame >> $newYUVFile
        done
 
-       rm frames*
+       i=i+count
+       count=0
    fi
 done
 
 echo  "scaling the video"
-ffmpeg -s:v 160x120 -r 5 -i ${newYUVFile} -vf scale=320:240 -c:v rawvideo -pix_fmt yuv422p out.yuv
-
+# ffmpeg -s:v 160x120 -r 5 -i ${newYUVFile} -vf scale=320:240 -c:v rawvideo -pix_fmt yuyv422 ${newYUVFile}
+#
 echo "convert yuv to .264 in mp4 container"
-ffmpeg -f rawvideo -vcodec rawvideo -s 640x480 -r 30 -pix_fmt yuyv422 -i output_640x480.yuv -c:v libx264 -preset ultrafast -qp 0 output.mp4
+# ffmpeg -f rawvideo -vcodec rawvideo -s 640x480 -r 30 -pix_fmt yuyv422 -i ${newYUVFile} -c:v libx264 -preset ultrafast -qp 0 ${newYUVFile}.mp4
 
-mv ./video_${fileCounter}* /var/www/video_in/combined
+# mv ./video_${fileCounter}* /var/www/video_in/combined
 
 # ffmpeg -f lavfi -i nullsrc=s=640x480:d=10:r=30 -i output_640x480.yuv -filter_complex "[0:v][1:v]overlay[video]" -map "[video]" -shortest output_video.yuv
 
