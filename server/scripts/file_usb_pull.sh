936abe0213c7c8b1196737aa90caab51abb2bf55
Your Name <you@example.com>
2018-04-17 15:07:53 +0530

    changes file for stability

--- server/scripts/file_usb_pull.sh
+++ server/scripts/file_usb_pull.sh
@@ -4,29 +4,30 @@
 
 tgtdir=`/usr/bin/adb shell "ls /sdcard/VectorsData/ ; exit 0" | head -n 1`
 
-echo "got tgtdir as #${tgtdir}#"
-
+mkdir -p /var/www/video_in/dSpace
+dSpace="/var/www/video_in/dSpace"
 dest_fldr="/var/www/video_in"
 import="${dest_fldr}/import"
 newLine="
 "
+
 timeAtDest=`date +%s`
 /usr/bin/adb pull /sdcard/VectorsData/ ${import}
 echo "attempted to pull content at $timeAtDest}"
 rcvdFiles=`ls ${import}/video*| grep -v json`
 outStr=""
 for file in ${rcvdFiles} ; do
-   mv ${file} ${dest_fldr}
-   mv ${file}.json ${dest_fldr}
-   fileName=$(basename ${file})
-   /usr/bin/adb shell "rm /sdcard/VectorsData/${fileName}*; exit 0"
-   echo "attempted to remove file ${fileName}"
-   echo "Moved ${file}"
-   if [ -f "${dest_fldr}/${fileName}" ] ; then
-       outStr=$"${outStr}${newLine}{\"filename\":\"${fileName}\",\"time\":${timeAtDest}},"
-   else
-       echo "Failed to move file ${fileName} - not generating an ack"
-   fi
+    cp ${file} ${dSpace}
+    mv ${file} ${dest_fldr}
+    mv ${file}.json ${dest_fldr}
+    fileName=$(basename ${file})
+    /usr/bin/adb shell "rm /sdcard/VectorsData/${fileName}*; exit 0"
+    echo "Moved ${file}"
+    if [ -f "${dest_fldr}/${fileName}" ] ; then
+        outStr=$"${outStr}${newLine}{\"filename\":\"${fileName}\",\"time\":${timeAtDest}},"
+    else
+        echo "Failed to move file ${fileName} - not generating an ack"
+    fi
 done
 
 echo "${outStr}" | cat - ${dest_fldr}/rcvdlist.txt > /tmp//rcvdlist.txt.temp && mv /tmp//rcvdlist.txt.temp ${dest_fldr}/rcvdlist.txt
@@ -38,7 +39,7 @@ origin=`hostname`
 echo "{\"ack_time\":${timeAtDest},\"traversal\":[{\"first\":${timeAtDest},\"second\":\"${origin}\"}],"items":[${str2}]}" > /tmp/ack_video_00.json
 /usr/bin/adb push /tmp/ack_video_00.json /sdcard/VectorsData/
 if [ "$?" -ne "0" ]; then
-      echo "Push of ack may have failed"
+    echo "Push of ack may have failed"
 fi
 echo "Tried to push the ack"
 
@@ -46,5 +47,3 @@ echo "Tried to push the ack"
 
 
 # date >> /var/spool/vector/run_out
-
-
