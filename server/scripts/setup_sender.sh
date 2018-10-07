906c6ad7c1389cd61898c4f4b0dd5d36ed951bca
arnav dhamija <arnav.dhamija@gmail.com>
2018-10-07 22:35:44 +0530

    Added elif code

--- server/scripts/setup_sender.sh
+++ server/scripts/setup_sender.sh
@@ -7,6 +7,12 @@ echo; read -p "Frequency of capture interval (minutes): " cron_time
 echo; read -p "Time between frames (seconds): " frame_time
 echo; read -p "TTL of video chunk files (hours): " ttl_time
 echo; read -p "Frames before job: " frame_number
+echo; read -p "Video quality (l/m): " vid_quality
+
+if  [ "$vid_quality" != "l" -a "$vid_quality" != "m" ]; then
+    vid_quality="l"
+fi
+
 config_file="config"
 rm $config_file
 touch $config_file
@@ -15,6 +21,7 @@ echo "cron_time="$cron_time >> $config_file
 echo "frame_time="$frame_time >> $config_file
 echo "ttl_time="$ttl_time >> $config_file
 echo "frame_number="$frame_number >> $config_file
+echo "vid_quality="$vid_quality >> $config_file
 
 setup.sh shm jsvm
 # * resolution
