60101cdcfcd3a5f6ec7b95e94784e75bdf0e5c39
arnav dhamija <arnav.dhamija@gmail.com>
2018-10-07 21:58:22 +0530

    Fixed sudo

--- server/scripts/setup_asroot.sh
+++ server/scripts/setup_asroot.sh
@@ -23,7 +23,7 @@
 #    Authors: Abhishek Thakur, Arnav Dhamija, Tejashwar Reddy G            #
 ############################################################################
 
-sudo apt install -y adb git
+sudo apt install -y adb git subversion
 filesBase=/var/spool/vectors
 sudo mkdir -p ${filesBase}/video_in/import
 sudo mkdir -p ${filesBase}/video_out
