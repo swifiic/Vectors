4f0f93a719d5b8098ccc390a19de86246987aaee
arnav dhamija <arnav.dhamija@gmail.com>
2018-10-07 23:27:06 +0530

    Added default values

--- server/scripts/setup_sender.sh
+++ server/scripts/setup_sender.sh
@@ -4,13 +4,23 @@
 # sh setup_asroot.sh
 # sh install_shvc.sh
 echo; read -p "Default webcam suffix to be used; enter X in /dev/videoX (e.g. enter '1' for '/dev/video1'): " webcam_suffix
-echo; read -p "Frequency of capture interval (minutes): " cron_time
+echo; read -p "Frequency of the capture cron job (minutes): " cron_time
 echo; read -p "Framerate (seconds): " frame_rate
 echo; read -p "TTL of video chunk files (hours): " ttl_time
-echo; read -p "Frames before job: " frame_number
+echo; read -p "Frames per job: " frame_number
 echo; read -p "Video quality (l/m): " vid_quality
 echo; read -p "Source node (last 4 digits): " src_node
 echo; read -p "Destination node (last 4 digits):" dest_node
+
+webcam_suffix="${webcam_suffix:-5}"
+cron_time="${cron_time:-10}"
+frame_rate="${frame_rate:-0.25}"
+ttl_time="${ttl_time:-24}"
+frame_number="${frame_number:-65}"
+vid_quality="${vid_quality:-"l"}"
+src_node="${src_node:-"aaaa"}"
+dest_node="${dest_node:-"bbbb"}"
+
 
 if  [ "$vid_quality" != "l" -a "$vid_quality" != "m" ]; then
     vid_quality="l"
@@ -29,6 +39,7 @@ echo "vid_quality="$vid_quality >> $config_file
 echo "src_node="$src_node >> $config_file
 echo "dest_node="$dest_node >> $config_file
 
+# (crontab -l ; echo "*/$cron_time * * * * scripts/rcvr_cron.sh")| crontab -
 # ./setup.sh shm jsvm
 # * resolution
 #     * check for V4L2 webcam
